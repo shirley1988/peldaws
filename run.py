fe195bf2100e45feb5764a66ad032a2c5df72e8c
Nana <shirley19881218@gmail.com>
2019-06-09 12:55:00 -0700

    commit before trying docker

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 
 from praat import app, init_db
 
-# Run server on port 80
+# Run server on port 8000
 if __name__ == '__main__':
     app.config.update(
         SECRET_KEY = '16f470fe142040d4def5597ab082c84d1b27eaf900ccaa8f74aa987328eaddc9',
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         GOOGLE_LOGIN_CLIENT_SECRET = 'B6mzHoERK5lYv-nRImfd2-1o',
         GOOGLE_LOGIN_REDIRECT_URI = 'http://peldawsnliu.azurewebsites.net/oauth2callback',
         STAGE = 'production',
-        SOTRAGE_LOCATION = 'azure',
+        SOTRAGE_LOCATION = 'local',
     )
     init_db()
     app.run(host='0.0.0.0', port=8000, threaded=True)
